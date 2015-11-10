a0034a380cc479445622094f26c92139e9f213a4
ImJessW <ImJessW@gmail.com>
2015-11-10 14:58:40 -0500

    Now pulls in data

--- hillaryapp/public/javascripts/hfa.js
+++ hillaryapp/public/javascripts/hfa.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
-	alert("hi")
+
+// var hilEvents=["events"]
+// 	$.ajax({
+// 	  dataType: "json",
+// 	  url: "https://s3.amazonaws.com/interview-api-samples/events-results.json",
+// 	  data: hilEvents,
+// 	  success: success
+// 	});
+
+	$.ajax(
+	{
+	    url: 'https://s3.amazonaws.com/interview-api-samples/events-results.json',
+	    cache: false,
+	    dataType: 'json',
+	    type: 'GET',
+	    data: ({ 'events' : [] }),
+	    success: hillEvents,
+	    error: function(e, xhr)
+	    {
+	        console.log(e);
+	    }
+
+	});
+
+	function hillEvents(data)
+		{
+		    $('#hillEvents').html(data.Date);
+		    console.log(data);
+		}
 
 	// $(".eventsButton").on("click",function(){
 	// 	console.log("success!")
