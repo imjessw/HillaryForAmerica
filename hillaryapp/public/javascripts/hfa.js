5373a64a492a6e48d6d18841efdb405337899a4b
ImJessW <ImJessW@gmail.com>
2015-12-17 20:49:35 -0500

    restructuring my code

--- hillaryapp/public/javascripts/hfa.js
+++ hillaryapp/public/javascripts/hfa.js
@@ -42,6 +42,50 @@ $(document).ready(function(){
 								
 
 	    $.each(data.events, function(i, evt){
+	    		    	// converting date and time.
+
+	    	var eventStart= evt.startDate;
+	    	var myDate = new Date(eventStart);
+			
+	    	// changing myDate to a date string
+			var dateString = 
+			  myDate.getUTCMonth() +"/"+
+			  ("0" + (myDate.getUTCDate()+1)).slice(-2) +"/"+
+			  ("0" + myDate.getUTCFullYear()).slice(-2);
+
+
+			// Converting time from military to standaerd and checking if it is am/pm
+			var hours = myDate.getUTCHours();
+			var minutes =myDate.getUTCMinutes();
+
+			var timeOfEvent = "" + ((hours >12) ? hours - 12 : hours);
+				timeOfEvent += (minutes < 10) ? ":0" + minutes : ":" + minutes;
+				timeOfEvent += (hours >= 12) ? " P.M." : " A.M.";
+
+
+
+
+			//Createing variables for schema found within locations
+
+
+				
+
+			var main_info = '<div class=info>'+'<h1 class="mainInfo">' + evt.name + '</h1>'
+	    	main_info += '<p class="mainInfo">' +  evt.templateInfo.title + '</p>';
+	    	main_info += '<p class="mainInfo">' + evt.description + '</p>';
+
+	    	// main_info += '<p class="mainInfo">' + evt.locations.city + ','+ evt.locations.state'</p>';
+            main_info += '<p class="mainInfo"> Date: ' + dateString + " "+ timeOfEvent+ '</p>';
+            main_info += '<button class="eventsButton main_info">View Event Details</button>';
+            main_info += '</div>';
+
+            var secondary_info = '<h1 class="secondary_info">' + 'hello'+ '</h1>';
+            secondary_info += '<p class="secondary_info">' + evt.description + '</p>';
+            secondary_info += '<p class="secondary_info">' + evt.createdDate + '</p>';
+            secondary_info += '<p class="secondary_info">' + evt.createdDate + '</p>';
+            secondary_info += '<p class="secondary_info">' + evt.guestsCanInviteOthers + '</p>';
+
+            console.log( evt.description)
 
 	    		evt.locations.forEach(function(locations, i, array) {
 
@@ -74,76 +118,21 @@ $(document).ready(function(){
 					var shiftEnd= evt.locations[i].shifts[i].endDate;
 
 
+					console.log(evt.locations[i].city)
+
 					//if equal to null/nan/undefined/""
 
 				    
 				});
 
-	    	// converting date and time.
 
-	    	var eventStart= evt.startDate;
-	    	var myDate = new Date(eventStart);
-			
-	    	// changing myDate to a date string
-			var dateString = 
-			  myDate.getUTCMonth() +"/"+
-			  ("0" + (myDate.getUTCDate()+1)).slice(-2) +"/"+
-			  ("0" + myDate.getUTCFullYear()).slice(-2);
-
-
-			// Converting time from military to standaerd and checking if it is am/pm
-			var hours = myDate.getUTCHours();
-			var minutes =myDate.getUTCMinutes();
-
-			var timeOfEvent = "" + ((hours >12) ? hours - 12 : hours);
-				timeOfEvent += (minutes < 10) ? ":0" + minutes : ":" + minutes;
-				timeOfEvent += (hours >= 12) ? " P.M." : " A.M.";
-
-
-
-
-			//Createing variables for schema found within 
-
-
-				
-
-			var main_info = '<div class=info>'+'<h1 class="mainInfo">' + evt.name + '</h1>'
-	    	main_info += '<p class="mainInfo">' +  evt.templateInfo.title + '</p>';
-	    	main_info += '<p class="mainInfo">' + evt.description + '</p>';
-
-	    	// main_info += '<p class="mainInfo">' + evt.locations.city + ','+ evt.locations.state'</p>';
-            main_info += '<p class="mainInfo"> Date: ' + dateString + " "+ timeOfEvent+ '</p>';
-            main_info += '<button class="eventsButton main_info">View Event Details</button>';
-            main_info += '</div>';
-
-            var secondary_info = '<h1 class="secondary_info">' + 'hello'+ '</h1>';
-            secondary_info += '<p class="secondary_info">' + evt.description + '</p>';
-            secondary_info += '<p class="secondary_info">' + evt.createdDate + '</p>';
-            secondary_info += '<p class="secondary_info">' + evt.createdDate + '</p>';
-            secondary_info += '<p class="secondary_info">' + evt.guestsCanInviteOthers + '</p>';
 			
 
 
        	});
-		console.log(hillEvents)
-
-            
-
-            // var official = evt.official;
-            // console.log(official;
-
-
-            // });
-
-            
-
-
-
         
 	}
+
 	console.log("done")
 
-
-
-}
-);
+});
